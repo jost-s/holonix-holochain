51d8eea935c4233295c68626d6ce6e9e53f07cff
Jost Schulte <jost.schulte@protonmail.com>
2024-05-19 12:19:37 -0600

    feat: add lair-keystore

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Developer environment shells to build Holochain apps.";
+  description = "Holochain flake";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
@@ -18,10 +18,16 @@
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # lair keystore
+    lair = {
+      url = "github:jost-s/holonix-lair";
+      flake = false;
+    };
   };
 
   # refer to flake-parts docs https://flake.parts/
-  outputs = inputs @ { self, nixpkgs, flake-parts, rust-overlay, crane, ... }:
+  outputs = inputs @ { self, nixpkgs, flake-parts, rust-overlay, crane, lair, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "aarch64-darwin" "x86_64-linux" "x86_64-darwin" "aarch64-linux" ];
 
@@ -44,16 +50,27 @@
             version = "workspace";
             src = craneLib.cleanCargoSource (craneLib.path ./.);
           };
+
+          lair-keystore = craneLib.buildPackage {
+            pname = "lair-keystore";
+            version = "workspace";
+            src = craneLib.cleanCargoSource lair;
+          };
         in
         {
           packages = {
             inherit holochain;
+            inherit lair-keystore;
             inherit rust;
           };
 
           devShells = {
             default = pkgs.mkShell {
-              packages = [ holochain rust ];
+              packages = [
+                holochain
+                lair-keystore
+                rust
+              ];
             };
           };
         };
