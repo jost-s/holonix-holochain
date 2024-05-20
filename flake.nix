753af29ed36d715dbb7fb69675d7122f32b771bc
Jost Schulte <jost.schulte@protonmail.com>
2024-05-20 16:36:14 -0600

    build(nix): add holochain build definition

--- flake.nix
+++ flake.nix
@@ -1,71 +1,108 @@
 {
-  description = "Holochain flake";
+  # flake format https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-flake.html#flake-format
+  description = "Holonix - Holochain Nix flake";
 
+  # specify all input dependencies needed to create the outputs of the flake
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
     # utility to iterate over multiple target platforms
     flake-parts.url = "github:hercules-ci/flake-parts";
 
-    # lib to build nix packages from rust crates
+    # lib to build a nix package from a rust crate
     crane = {
       url = "github:ipetkov/crane";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # rustup, rust and cargo
+    # Rust toolchain
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # lair keystore
-    lair = {
+    # Holochain sources
+    holochain-src = {
+      url = "github:holochain/holochain";
+      flake = false;
+    };
+
+    # Lair keystore sources
+    lair-keystore-src = {
       url = "github:holochain/lair";
-      # url = "github:jost-s/holonix-lair";
       flake = false;
     };
   };
 
-  # refer to flake-parts docs https://flake.parts/
-  outputs = inputs @ { self, nixpkgs, flake-parts, rust-overlay, crane, lair, ... }:
+  # outputs that this flake should produce
+  outputs = inputs @ { self, nixpkgs, flake-parts, rust-overlay, crane, holochain-src, lair-keystore-src, ... }:
+    # refer to flake-parts docs https://flake.parts/
     flake-parts.lib.mkFlake { inherit inputs; } {
+      # systems that his flake can be used on
       systems = [ "aarch64-darwin" "x86_64-linux" "x86_64-darwin" "aarch64-linux" ];
 
+      # for each system...
       perSystem = { config, pkgs, system, ... }:
         let
+          # include Rust overlay in nixpkgs
           overlays = [ (import rust-overlay) ];
           pkgs = import nixpkgs {
             inherit system overlays;
           };
 
+          # define Rust toolchain version and targets to be used in this flake
           rust = (pkgs.rust-bin.stable."1.78.0".default.override
             {
               targets = [ "wasm32-unknown-unknown" ];
             });
 
-          # use rust toolchain specified above
+          # instruct crane to use Rust toolchain specified above
           craneLib = (crane.mkLib pkgs).overrideToolchain rust;
 
-          holochain = craneLib.buildPackage {
-            pname = "holochain";
-            version = "workspace";
-            src = craneLib.cleanCargoSource (craneLib.path ./.);
-          };
+          # define how to build Holochain binaries
+          holochain =
+            let
+              # Crane filters out all non-cargo related files. Define include filter with files needed for build.
+              nonCargoBuildFiles = path: _type: builtins.match ".*(json|sql|wasm.gz)$" path != null;
+              includeFilesFilter = path: type:
+                (craneLib.filterCargoSources path type) || (nonCargoBuildFiles path type);
+            in
+            craneLib.buildPackage {
+              pname = "holochain";
+              version = "workspace";
+              # Use Holochain sources as defined in input dependencies and include only those files defined in the
+              # filter previously.
+              src = pkgs.lib.cleanSourceWith {
+                src = holochain-src;
+                filter = includeFilesFilter;
+              };
+              # additional packages needed for build
+              buildInputs = [
+                pkgs.perl
+                pkgs.go
+              ];
+              # do not check built package as it either builds successfully or not
+              doCheck = false;
+            };
 
+          # define how to build Lair keystore binary
           lair-keystore =
             let
-              sqlFilter = path: _type: builtins.match ".*(sql|md)$" path != null;
-              sqlOrCargo = path: type:
-                (sqlFilter path type) || (craneLib.filterCargoSources path type);
+              # Crane filters out all non-cargo related files. Define include filter with files needed for build.
+              nonCargoBuildFiles = path: _type: builtins.match ".*(sql|md)$" path != null;
+              includeFilesFilter = path: type:
+                (craneLib.filterCargoSources path type) || (nonCargoBuildFiles path type);
             in
             craneLib.buildPackage {
               pname = "lair-keystore";
               version = "workspace";
+              # Use Lair keystore sources as defined in input dependencies and include only those files defined in the
+              # filter previously.
               src = pkgs.lib.cleanSourceWith {
-                src = lair;
-                filter = sqlOrCargo;
+                src = lair-keystore-src;
+                filter = includeFilesFilter;
               };
+              # additional packages needed for build
               # perl needed for openssl on all platforms
               buildInputs = [ pkgs.perl ]
                 ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin [
@@ -75,6 +112,7 @@
                 # additional packages needed for darwin platforms on x86_64
                 pkgs.darwin.apple_sdk_11_0.frameworks.CoreFoundation
               ]);
+              # do not check built package as it either builds successfully or not
               doCheck = false;
             };
         in
